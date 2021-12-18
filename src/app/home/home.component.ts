c6e70d3bf86d1bea4577235930c4ef70762ea485
LUCÍA NAVARRO SÁNCHEZ <Lucia.Navarro5@alu.uclm.es>
2021-12-18 19:42:07 +0100

    añadiendo funcionalidad a la aplicacion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { environment } from '@env/environment';
 import { finalize } from 'rxjs/operators';
 import { ShortenerService } from './shortener.service';
+import { ToastController } from '@ionic/angular';
 
 interface URLCard {
+  title: string;
   long_url: string;
   short_url: string;
   created_at: number;
+  counter: number;
 }
 
 @Component({
@@ -20,7 +23,7 @@ export class HomeComponent implements OnInit {
   inputURL: any = '';
   URLCards: URLCard[] = [];
 
-  constructor(private shortenerService: ShortenerService) {}
+  constructor(private shortenerService: ShortenerService, public toastCtrl: ToastController) {}
 
   ngOnInit() {
     this.isLoading = false;
@@ -38,21 +41,68 @@ export class HomeComponent implements OnInit {
   }
 
   shortURL(url: string) {
+    if (url == '') {
+      this.showToastMsg('No has introducido ningún enlace 🙄');
+    }
     console.log(url);
     this.shortenerService.postURL(url).subscribe((result: any) => {
       console.log(result);
 
       this.URLCards.push({
+        title: this.title(url),
         long_url: result.url,
         short_url: environment.serverUrl + '/' + result.code,
         created_at: result.created_at,
+        counter: 0,
       });
       this.inputURL = '';
+      this.showToastMsg('URL acortada correctamente 🤗');
     });
   }
 
-  copyURL(url: string) {
-    console.log('Copied');
-    // this.clipboardService.copyFromContent(url)
+  title(url: any) {
+    var title = url.split('/', 3);
+    return title[2];
+  }
+
+  orderDates() {
+    this.URLCards.reverse();
+  }
+
+  updateCounter(card: URLCard) {
+    var aux: any;
+    for (aux in this.URLCards) {
+      if (aux.long_url == card.long_url) {
+        aux.counter++;
+      }
+    }
+    return this.URLCards;
+  }
+
+  public copyForClipboard(event: MouseEvent, short_url: string): void {
+    event.preventDefault();
+    const payload: string = short_url;
+    this.showToastMsg('Copiado al portapapeles 👍🏼');
+
+    let listener = (e: ClipboardEvent) => {
+      let clipboard = e.clipboardData || window['clipboardData'];
+      clipboard.setData('text', payload.toString());
+      e.preventDefault();
+    };
+
+    document.addEventListener('copy', listener, false);
+    document.execCommand('copy');
+    document.removeEventListener('copy', listener, false);
+  }
+  showToastMsg(message: string) {
+    this.toastCtrl
+      .create({
+        message: message,
+        duration: 1800,
+      })
+      .then((toastRes) => {
+        console.log(toastRes);
+        toastRes.present();
+      });
   }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,23 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { IonicModule } from '@ionic/angular';
 
+import { I18nModule } from '@app/i18n';
 import { SharedModule } from '@shared';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, TranslateModule, SharedModule, IonicModule, HomeRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    TranslateModule,
+    SharedModule,
+    IonicModule,
+    HomeRoutingModule,
+    FormsModule,
+    I18nModule,
+  ],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
